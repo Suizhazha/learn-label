4815302d259043232074f5b736ed5d4ea014fb58
Suizhazha <573505403@qq.com>
2021-04-09 00:12:26 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,15 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:'/home',
+    //路由懒加载
+    component:()=>import('../views/Home.vue'),
+child:[{
+  path:'/son',
+  component:()=>import('../views/Son')
+}]
+  }
 ]
 
 const router = new VueRouter({
