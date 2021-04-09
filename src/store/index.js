fcaa1b6a5176b03a36c46db3ad679e185262b87d
Suizhazha <573505403@qq.com>
2021-04-09 17:44:45 +0800

    模块化

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import count from "@/store/count"
 
 Vue.use(Vuex)
 
 console.log(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    count:5
-  },
-  mutations: {
-    add(){
-      this.state.count++
-    }
-  },
-  actions: {
-    add(context){
-      setTimeout(()=>{
-        context.commit('add')
-      },2000)
-    }
-  },
+
   modules: {
+    count
   }
 })
